a541337e9a13ad9051f75ddf15cf06391769b8c8
Pablo Helmbrecht <pablohelmbrecht@gmail.com>
2023-03-09 14:32:59 +0000

    Update page.tsx

--- app/buyerpersona/page.tsx
+++ app/buyerpersona/page.tsx
@@ -8,6 +8,8 @@ import {
   BarList
 } from '@tremor/react';
 
+import { ArrowUpOnSquareIcon } from '@heroicons/react/24/outline';
+
 const categories: {
   title: string;
   metric: string;
@@ -33,6 +35,20 @@ const categories: {
 export default function PlaygroundPage() {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <Flex justifyContent="justify-center" marginTop="mt-12">
+        <Metric>Generador de Buyer Persona</Metric>
+      </Flex>
+      <Flex justifyContent="justify-center" marginTop="mt-4">
+        <Text textAlignment="text-center">
+          Haz click en el botón debajo para subir el archivo .CSV
+        </Text>
+      </Flex>
+      <Flex justifyContent="justify-center" marginTop="mt-8">
+        <button className="tremor-base input-elem tr-flex-shrink-0 gap-2 tr-inline-flex tr-items-center tr-group focus:tr-outline-none focus:tr-ring-2 focus:tr-ring-offset-2 focus:tr-ring-transparent tr-font-medium tr-rounded-md tr-border tr-shadow-sm tr-pl-4 tr-pr-4 tr-pt-2.5 tr-pb-2.5 tr-text-lg tr-text-white tr-bg-blue-500 tr-border-transparent focus:tr-ring-blue-400 tr-text-white hover:tr-bg-blue-600 hover:tr-border-blue-600">
+          <ArrowUpOnSquareIcon className="h-6 w-6" />
+          <span> Subir archivo .CSV</span>
+        </button>
+      </Flex>
       <ColGrid numColsSm={2} numColsLg={3} gapX="gap-x-6" gapY="gap-y-6">
         {categories.map((item) => (
           <Card key={item.title}>
